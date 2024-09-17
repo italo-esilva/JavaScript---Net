3e21772622a4ab0eb836ebbb5e14b07340209676
Ítalo Silva <italoesilva15@gmail.com>
2024-09-17 16:54:36 -0300

    Add files via upload

---
+++ JavaScript - ÍTALO/JavaScript/script.js
@@ -0,0 +1,10 @@
+
+// Seleciona o botão com o ID 'CTA-BUTTOM'
+
+const ctaButtom = document.getElementById('cta-button')
+
+//Adiciona um evento de clique ao botão
+
+ctaButtom.addEventListener('click', function() {
+    alert('Você será um super progamador!');
+});
