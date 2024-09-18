5ff0f67af905bdf45a580fc74cf82763c3fc3464
Ítalo Silva <italoesilva15@gmail.com>
2024-09-18 16:50:24 -0300

    Add files via upload

---
+++ HTML SIMPLES/conceitos js/script.js
@@ -0,0 +1,7 @@
+let x = 10
+console.log("O valor de x é:" + x); /* Exibe uma mensagem */
+
+window.alert("Não deu certo :("); /*Exibe um alerta*/
+
+let nome = window.prompt("Qual é o seu nome?"); /*Permitirá a entrada do dado e o console mostrará a saída*/
+console.log("Nome informado:" + nome);
